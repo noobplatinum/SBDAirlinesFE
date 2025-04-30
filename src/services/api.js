aff5e47ef7a66320e627a1a6e55cadfd6f326059
J David P <jesayadavidgnp@gmail.com>
2025-04-30 15:04:03 +0700

    FAK

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-sbd-9.vercel.app/store/getAll'; 
+const API_URL = 'https://backend-sbd-9.vercel.app'; 
 
 const api = axios.create({
   baseURL: API_URL,
